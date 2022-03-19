f3977b22b5dd4cb0b9cf009fd65da59249a7224b
Jack Leow <jleow@goodrx.com>
2022-03-19 10:09:48 -0700

    Removed unused code.

--- src/SyntaxHighlight/Theme/Common.elm
+++ src/SyntaxHighlight/Theme/Common.elm
@@ -1,8 +1,6 @@
 module SyntaxHighlight.Theme.Common exposing (..)
 
 import Css exposing (Color, Style, fontStyle, fontWeight)
-import Css.Global exposing (Snippet, class)
-import SyntaxHighlight.Model exposing (Theme)
 
 
 noStyle : Style
@@ -27,22 +25,3 @@ italic style = Css.batch [ style, fontStyle Css.italic ]
 
 bold : Style -> Style
 bold style = Css.batch [ style, fontWeight Css.bold ]
-
-
-toCss : Theme -> List Snippet
-toCss theme =
-  List.map
-  ( \(name, style) -> class name [ style ] )
-  [ ( "elmsh", theme.default )
-  , ( "elmsh-hl", theme.selection )
-  , ( "elmsh-add", theme.addition )
-  , ( "elmsh-del", theme.deletion )
-  , ( "elmsh-comm", theme.comment )
-  , ( "elmsh1", theme.number )
-  , ( "elmsh2", theme.string )
-  , ( "elmsh3", theme.keyword )
-  , ( "elmsh4", theme.declarationKeyword )
-  , ( "elmsh5", theme.functionDeclaration )
-  , ( "elmsh6", theme.literal )
-  , ( "elmsh7", theme.functionArgument )
-  ]
