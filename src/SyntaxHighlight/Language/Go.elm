cb933a2772f7b4cb3f1811c556883974c1fd0f2b
Jack Leow <jleow@goodrx.com>
2022-06-24 22:18:41 -0700

    Support for Go "any", moved "interface", "struct" out of declarationKeywords list.

--- src/SyntaxHighlight/Language/Go.elm
+++ src/SyntaxHighlight/Language/Go.elm
@@ -24,20 +24,21 @@ go =
     , "goto"
     , "if"
     , "import"
+    , "interface"
     , "package"
     , "range"
     , "return"
     , "select"
+    , "struct"
     , "switch"
     ]
-  , declarationKeywords = Set.fromList [ "const", "interface", "struct", "var"
-    -- TODO remove
-    , "type" ]
+  , declarationKeywords = Set.fromList [ "const", "var", "type" ]
   , literalKeywords =
     Set.fromList [ "true", "false", "nil", "iota" ]
   , builtIns =
     Set.fromList
-    [ "bool", "byte", "rune", "string"
+    [ "any" -- Starting Go 1.18
+    , "bool", "byte", "rune", "string"
     , "int", "int8", "int16", "int32", "int64"
     , "uint", "uint8", "uint16", "uint32", "uint64", "uintptr"
     , "float32", "float64", "complex64", "complex128"
